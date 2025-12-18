2ad877ef36409cf168b7888131a4d56df6772ce1
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-12-18 16:21:16 +0530

    add: thread route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { userRouter } from "./user.route.js";
+import { threadRouter } from "./thread.route.js";
 
 export const apiRouter = Router();
 
 apiRouter.use("/me", userRouter);
+apiRouter.use("/threads", threadRouter);
