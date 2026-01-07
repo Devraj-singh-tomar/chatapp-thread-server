ceba7c50aa4ebf6e805928d0b53406c7b9e2b8b5
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2026-01-07 21:49:31 +0530

    feat: add upload functionality with Cloudinary integration and multer support

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { userRouter } from "./user.route.js";
 import { threadRouter } from "./thread.route.js";
 import { notificationsRouter } from "./notification.route.js";
 import { chatRouter } from "./chat.route.js";
+import { uploadRouter } from "./upload.route.js";
 
 export const apiRouter = Router();
 
@@ -10,3 +11,4 @@ apiRouter.use("/me", userRouter);
 apiRouter.use("/threads", threadRouter);
 apiRouter.use("/notifications", notificationsRouter);
 apiRouter.use("/chat", chatRouter);
+apiRouter.use("/upload", uploadRouter);
