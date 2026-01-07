a3626c731c87ee48e7972756ec85785afa6d561f
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2026-01-07 16:50:34 +0530

    feat: add create and list direct messaging services with chat routes.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { userRouter } from "./user.route.js";
 import { threadRouter } from "./thread.route.js";
 import { notificationsRouter } from "./notification.route.js";
+import { chatRouter } from "./chat.route.js";
 
 export const apiRouter = Router();
 
 apiRouter.use("/me", userRouter);
 apiRouter.use("/threads", threadRouter);
 apiRouter.use("/notifications", notificationsRouter);
+apiRouter.use("/chat", chatRouter);
